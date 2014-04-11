2ffbe91ce11df6e11a016fb45792d7a88b4485f2
Brian McCallister <brianm@skife.org>
2014-04-10 21:36:33 -0700

    better names

--- src/test/java/org/skife/clocked/ClockedExecutorServiceTest.java
+++ src/test/java/org/skife/clocked/ClockedExecutorServiceTest.java
@@ -14,10 +14,10 @@ public class ClockedExecutorServiceTest
     {
         try (ClockedExecutorService clock = new ClockedExecutorService(2))
         {
-            clock.schedule(NOOP, 10, TimeUnit.MILLISECONDS);
-            clock.schedule(NOOP, 5, TimeUnit.MILLISECONDS);
-            clock.schedule(NOOP, 15, TimeUnit.MILLISECONDS);
-            clock.schedule(NOOP, 20, TimeUnit.MILLISECONDS);
+            clock.schedule(COUNT, 10, TimeUnit.MILLISECONDS);
+            clock.schedule(COUNT, 5, TimeUnit.MILLISECONDS);
+            clock.schedule(COUNT, 15, TimeUnit.MILLISECONDS);
+            clock.schedule(COUNT, 20, TimeUnit.MILLISECONDS);
 
             clock.advance(5, TimeUnit.MILLISECONDS).get();
             assertThat(counter.get()).isEqualTo(1);
@@ -38,16 +38,16 @@ public class ClockedExecutorServiceTest
     {
         try (ClockedExecutorService clock = new ClockedExecutorService(2))
         {
-            clock.scheduleAtFixedRate(NOOP, 5, 5, TimeUnit.MILLISECONDS);
+            clock.scheduleAtFixedRate(COUNT, 5, 5, TimeUnit.MILLISECONDS);
             clock.advance(7, TimeUnit.MILLISECONDS);
         }
     }
 
     public static final AtomicLong counter = new AtomicLong(0);
-    public static final Runnable NOOP = new Runnable()
+    public static final Runnable COUNT = new Runnable()
     {
         @Override
-        public synchronized void run()
+        public void run()
         {
             counter.incrementAndGet();
         }
